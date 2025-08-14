7fb81a33ee9fbeb4fcc817e816bb504d0981bf3b
Sohrab Rezaei <106667528+Sohrab-rz@users.noreply.github.com>
2025-08-14 11:59:03 -0400

    Update TLBO_MAX.m

--- TLBO/TLBO_MAX.m
+++ TLBO/TLBO_MAX.m
@@ -1,7 +1,5 @@
 %______________________________________________________________________%
     % ------------  Creator    :      Sohrab Rezaei         ----------- %
-    % ------------  K. N. Toosi University of Technology ----------- % 
-    % ------------      Advanced Control Systems Lab     ----------- %
     % -------------------- TLBO - Maximization --------------------- %
 %_____________________________________________________________________%
 
@@ -140,4 +138,5 @@ contourf(X,Y,z,20)
 colorbar
 hold on
 plot(in_new_1(best_solution_position(1,1),1),in_new_1(best_solution_position(1,1),2),'rx','markersize',7);
+
 
